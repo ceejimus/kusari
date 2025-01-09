1bd4db148df1a4e3855beb8b18dc0256d12b7f19
cj <cj@atmoscape.net>
2025-01-09 14:22:51 -0600

    [dev] complete file upsert SQL

--- sql/upsert_filestate.sql
+++ sql/upsert_filestate.sql
@@ -2,3 +2,10 @@ INSERT INTO file_state
   (path, hash, size, timestamp, modtime)
 VALUES
   (:path, :hash, :size, :timestamp, :modtime)
+ON CONFLICT
+  (path, hash)
+DO UPDATE SET
+  size=excluded.size
+ ,timestamp=excluded.timestamp
+ ,modtime=excluded.modtime
+;
